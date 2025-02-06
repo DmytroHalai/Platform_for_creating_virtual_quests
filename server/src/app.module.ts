298e7b59c0cc87a762d4f57c2ad74a74e5c3cc45
DmytroHalai <halai.dmytro@lll.kpi.ua>
2025-02-06 10:04:08 +0200

    start creation of database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { QuestsModule } from './quests/quests.module';
+import { TasksModule } from './tasks/tasks.module';
+import { ProgressModule } from './progress/progress.module';
+import { RatingModule } from './rating/rating.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, QuestsModule, TasksModule, ProgressModule, RatingModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
