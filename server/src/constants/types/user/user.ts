da6e85a0f2c898ccc6d652e8d423e15097f68a0f
Solomka Andrii <andriissolomka@gmail.com>
2025-02-08 12:56:08 +0200

    added IGoogle interface

--- server/src/constants/types/user/user.ts
+++ server/src/constants/types/user/user.ts
@@ -1,3 +1,11 @@
 export interface IUser {
   user_id: number;
 }
+
+export interface IGoogleUser {
+  email: string;
+  username: string;
+  role: string;
+  password: string;
+  isEmailConfirmed: boolean;
+}
