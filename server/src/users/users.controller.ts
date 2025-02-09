69b725e86033bd3929e447af3a4e02c42ec2a4dd
Solomka Andrii <andriissolomka@gmail.com>
2025-02-10 01:00:50 +0200

    some fixes

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -10,12 +10,15 @@ import {
   Request,
   Response,
   Query,
+  Put,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CookieService } from 'src/cookie/cookie.service';
+import { GetUser } from 'src/common/decorators/getUser';
+import { IUser } from 'src/constants/types/user/user';
 
 @Controller('users')
 export class UsersController {
@@ -47,6 +50,36 @@ export class UsersController {
   async countAllActiveUsers(@Response() res) {
     const count = await this.usersService.countAllActiveUsers();
     return res.send({ count });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async findProfile(@Response() res, @GetUser() id: IUser) {
+    const user = await this.usersService.findProfile(+id);
+    return res.send({ user });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch()
+  async update(
+    @GetUser() id: IUser,
+    @Body() updateUserDto: UpdateUserDto,
+    @Response() res,
+  ) {
+    const update = await this.usersService.update(+id, updateUserDto);
+    return res.send({ update });
+  }
+
+  @Get('rating')
+  async selectRating(@Response() res) {
+    const users = await this.usersService.selectRating();
+    return res.send({ users });
+  }
+
+  @Get(':username')
+  async findByName(@Response() res, @Param('username') username: string) {
+    const user = await this.usersService.findByName(username);
+    return res.send({ user });
   }
 
   // @Get(':id')
